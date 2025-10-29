1eb64a4029d15bb305081d7855be4585dacd8f94
bill291104 <bill29123@gmail.com>
2025-10-29 14:44:09 +0900

    chore(project): init project dependency

---
+++ src/main/java/com/team_pingpong/team_pingpong/TeamPingpongApplication.java
@@ -0,0 +1,13 @@
+package com.team_pingpong.team_pingpong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamPingpongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamPingpongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team_pingpong/team_pingpong/TeamPingpongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team_pingpong.team_pingpong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamPingpongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
